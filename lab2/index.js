64201d19069ce954686d464969776449561a4ed5
omontel1 <70669023+omontel1@users.noreply.github.com>
2021-01-30 23:34:04 -0500

    lab2

---
+++ lab2/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+axios('/user?ID=12345')
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+import { } from './Fetch.js';
+    
+    let val = pokemon.name;
+    console.log(pokemon.id);
