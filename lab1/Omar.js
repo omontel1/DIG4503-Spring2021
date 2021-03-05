bfef171d21d78e98de8d0d96426e16565a38ad20
omontel1 <70669023+omontel1@users.noreply.github.com>
2021-03-04 22:09:17 -0500

    Created Lab 1

---
+++ lab1/Omar.js
@@ -0,0 +1,9 @@
+import Person from './Person.js';
+
+class Omar extends Person {
+    constructor(name, favoriteColor) {
+        super(name, favoriteColor);
+    }
+}
+
+export default Omar;
---
+++ lab1/index.js
@@ -0,0 +1,4 @@
+import Omar from './Omar.js';
+
+const o = new Omar("Omar", "#FF0000");
+o.speak();
